188f9d8ca2e1a1d683355d80bad8c823d980fd63
paul_satter <7494717+paul_satter@user.noreply.gitee.com>
2021-12-18 13:59:04 +0800

    initial

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import { defineComponent } from 'vue';
+import { btnProps, ButtonTypes } from './types';
+import './index.scss';
+const props = btnProps();
+
+export default defineComponent({
+  name: 'AfButton',
+  props: props,
+  setup(props, { emit, attrs }) {
+    return () => {
+      const { btnType, href, label, size, disabled, backgroundColor } = props;
+      const classes = classNames('btn', {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        ['disabled']: btnType !== ButtonTypes.LINK && disabled,
+      });
+
+      const styles = {
+        backgroundColor: backgroundColor,
+      };
+      if (btnType === ButtonTypes.LINK && href) {
+        return (
+          <a {...attrs} href={href} class={classes} style={styles}>
+            {label}
+          </a>
+        );
+      } else {
+        return (
+          <button {...attrs} disabled={disabled} class={classes} style={styles}>
+            {label}
+          </button>
+        );
+      }
+    };
+  },
+});
